e60b169066969781627e632555195b32e8cbefaf
k3jo <joanne.kwh@gmail.com>
2017-02-25 16:58:18 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+	// $('label').on('click', function(){
+	// 	$(this).addClass(selected-bg);
+	// });
+
+	// $('label').on('click', function(){
+	// 	var checkBox = $('label').find('.emoji-group');
+	// 	$('label').removeClass('.selected-bg');
+	// 	checkBox.toggleClass(".selected-bg");
+	// });
+
+
+
+
+	// $('label').on('click', function(){
+	// 	$('svg').toggleClass('picked');
+	// });
+
+
+});

f991df997ab8e45588fedd1c5b93b4476cdcc89f
k3jo <joanne.kwh@gmail.com>
2017-02-25 21:23:48 -0500

    jquery

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ $(document).ready(function(){
 
 
 	$('label').on('click', function(){
-		$('.emoji-group').removeClass('selected-bg');
+		var groupClass = $(this).attr('class');
+		$('.' + groupClass + ' .emoji-group').removeClass('selected-bg');
 		$(this).find('.emoji-group').addClass('selected-bg');
+		console.log(groupClass);
 	});
 
+
 	$('label').on('click', function(){
-		$('svg .picked').css('fill', '#58595B');
+
+		var otherGroup = $(this).attr('class');
+		 $('.' + otherGroup + ' .picked').css('fill', '#58595B');
 		$(this).find('.picked').css('fill', '#FFDE17');
 	});
 

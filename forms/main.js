8699f937469d04a99bf4801eb595d399179fcaf7
k3jo <joanne.kwh@gmail.com>
2017-08-04 17:44:01 -0400

    the toggled load in

--- forms/main.js
+++ forms/main.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
 
+//background change
 	$('label').on('click', function(){
 		var groupClass = $(this).attr('class');
 		$('.' + groupClass + ' .emoji-group').removeClass('selected-bg');
 		$(this).find('.emoji-group').addClass('selected-bg');
-		console.log(groupClass);
+	});
+	//.row-2 .emoji-group
+
+	//upon loading saved selections, toggled classes load in
+	$('input').each(function(i){
+		var superClass = $(this).parent().parent().attr('class');
+		$('.' + superClass + ' .emoji-group').addClass('selected-bg');
+		$('.' + superClass + ' .picked').css('fill', '#FFDE17');
+		// console.log(i);
 	});
 
 
+
+//fill change
 	$('label').on('click', function(){
 
 		var otherGroup = $(this).attr('class');
